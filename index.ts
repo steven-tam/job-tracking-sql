cfbc1dbae4b40909c40e764aaa5d8dca81e1290d
Steven Tam <60487779+steven-tam@users.noreply.github.com>
2025-03-01 20:56:17 -0600

    edit comments

--- index.ts
+++ index.ts
@@ -10,13 +10,13 @@ db.exec("PRAGMA journal_mode = WAL;");
 // createTable(db)
 
 
-// For making changes in database
+// For changing the "job_search" table
 //----------- Query Function References ------------
 
 // deleteJob(db, 2)
 // updateStatus(db, 1, "interview")
 // insertJob(db, "Position", "Company", "Industry", "Role", "Date Posted", "Date Applied", "Source", "Status")
 
-//----------- Execute Query Dunctions Below ------------
+//----------- Execute Query Functions Below ------------
 
 insertJob(db, "Software Engineering Intern", "Endeavour. Inspired Infrastructure", "Utilities and Renewable Energy", "Intern", "Feb 26, 2025", "Mar 1, 2025", "Handshake", "applied")
